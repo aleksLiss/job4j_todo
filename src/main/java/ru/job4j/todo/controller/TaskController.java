21989bab7b64c5eda03389687fd2f89894dfb5eb
aleks <darklissik.darklissik@gmail.com>
2025-07-25 12:01:37 +0300

    update TaskController.java

--- src/main/java/ru/job4j/todo/controller/TaskController.java
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -3,68 +3,110 @@ package ru.job4j.todo.controller;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.todo.model.Task;
 import ru.job4j.todo.service.TaskService;
 
 import java.util.List;
+import java.util.Optional;
 
-@Controller
 @ThreadSafe
-@RequestMapping("/task")
+@RequestMapping("/tasks")
+@Controller
 public class TaskController {
 
     private final TaskService taskService;
 
-    @RequestMapping("/list/all")
-    public String getAllTasks(Model model) {
-        try {
-            List<Task> allTasks = taskService.getAll();
-            model.addAttribute("tasks", allTasks);
-        } catch (Exception ex) {
-            model.addAttribute("error", "Ошибка извлечения всех задач");
-            return "errors/404";
-        }
-        return "tasks/allList";
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
     }
 
-    @RequestMapping("/list/new")
+    @GetMapping("/list/all")
+    public String getAllTasks(Model model) {
+        List<Task> allTasks = taskService.getAll();
+        try {
+            model.addAttribute("tasks", allTasks);
+            return "tasks/allList";
+        } catch (Exception ex) {
+            model.addAttribute("message", "Ошибка извлечения всех задач");
+            return "errors/404";
+        }
+    }
+
+    @GetMapping("/list/new")
     public String getNewTasks(Model model) {
         try {
             List<Task> newTasks = taskService.getNewTasks();
             model.addAttribute("tasks", newTasks);
         } catch (Exception ex) {
-            model.addAttribute("error", "Ошибка извлечения новых задач");
+            model.addAttribute("message", "Ошибка извлечения новых задач");
             return "errors/404";
         }
         return "tasks/newList";
     }
 
-    @RequestMapping("/list/done")
+    @GetMapping("/list/done")
     public String getDoneTasks(Model model) {
         try {
             List<Task> doneTasks = taskService.getDoneTasks();
             model.addAttribute("tasks", doneTasks);
         } catch (Exception ex) {
-            model.addAttribute("erorr", "Ошибка извлечения выполненных задач");
+            model.addAttribute("message", "Ошибка извлечения выполненных задач");
             return "errors/404";
         }
         return "tasks/doneList";
     }
 
-    @RequestMapping("/*{id}")
-    public String getTask() {
+    @GetMapping("/{id}")
+    public String getOneTaskById(Model model, @PathVariable int id) {
         try {
-            // переделать что б возвращало Optional<Task>
-
-
+            Optional<Task> foundTask = taskService.getById(id);
+            if (foundTask.isEmpty()) {
+                throw new RuntimeException();
+            }
+            model.addAttribute("task", foundTask.get());
         } catch (Exception ex) {
-
+            model.addAttribute("message", "Ошибка извлечения задачи");
+            return "errors/404";
         }
         return "tasks/one";
     }
 
+    @PostMapping("/update")
+    public String updateTask(Model model, @ModelAttribute Task task) {
+        taskService.update(task);
+        return "redirect:/tasks/list/all";
+    }
 
+    @GetMapping("/delete/{id}")
+    public String deleteById(Model model, @PathVariable int id) {
+        taskService.delete(id);
+        return "redirect:/tasks/list/all";
+    }
 
+    @PostMapping("/create")
+    public String createTask(@ModelAttribute Task task) {
+        taskService.save(task);
+        return "redirect:/tasks/list/all";
+    }
 
+    @GetMapping("/create")
+    public String getCreateTask(@ModelAttribute Task task) {
+        return "tasks/create";
+    }
+
+    @GetMapping("/update/{id}")
+    public String getUpdateTask(Model model, @PathVariable int id) {
+        Optional<Task> foundTask = taskService.getById(id);
+        try {
+            if (foundTask.isEmpty()) {
+                throw new RuntimeException();
+            }
+            model.addAttribute("task", foundTask.get());
+            return "tasks/update";
+        } catch (Exception ex) {
+            model.addAttribute("message", "Ошибка редактирования задачи");
+            return "errors/404";
+        }
+    }
 }
